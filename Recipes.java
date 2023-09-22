a16be3577b823ea599853a666513a9e38574b1c3
JacksonRCode <jackdareid@gmail.com>
2023-09-21 21:28:42 -0400

    Play with how I want to store recipes. Need to fix bug with recipes that either stops them from being added or printed.
    Need to create ArrayList data structure for practice and so I can tailor
    the methods.

--- Recipes.java
+++ Recipes.java
@@ -1,13 +1,48 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Recipes {
-    public static void main(String[] args) {
-        Scanner scanObj = new Scanner(System.in);
-        System.out.println("Enter recipe name: ");
 
-        String recipe = scanObj.nextLine();
-        System.out.println(recipe);
-        scanObj.close();
+    ArrayList<String> recipeList = new ArrayList<String>();
+    int recipeCount = 0;
 
+    public Recipes() {
+        this.recipeList = new ArrayList<>();
     }
+
+    public void addRecipe(String name) {
+        recipeList.add(name);
+        recipeCount++;
+    }
+
+    public void deleteRecipe(String name) {
+        recipeList.remove(name);
+        recipeCount--;
+    }
+
+    public String toString() {
+        String string = "";
+        for (int i=0; i < recipeCount-1; i++) {
+            string += (String) recipeList.get(i) + ", ";
+        }
+        if (string!="") {
+            string = "[" + string.substring(0, string.length() - 2) + "]";
+        }
+        else {
+            return "[]";
+        }
+        return string;
+    }
+    
+    // public static void main(String[] args) {
+    //     Scanner scanObj = new Scanner(System.in);
+    //     System.out.println("Enter recipe name: ");
+
+    //     String recipe = scanObj.nextLine();
+    //     System.out.println(recipe);
+    //     scanObj.close();
+
+    // }
+
+    
 }
